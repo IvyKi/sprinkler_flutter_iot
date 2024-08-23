e4f1b0aa6a3758c29b9cee3226b44d439863407a
Ivy Ki <144656120+IvyKi@users.noreply.github.com>
2024-08-23 13:25:09 +0900

    feature: connect with supabase
    connected successfully table
    - sprinkler_get
    - sprinkler-get2
    - sprinkler-get3
    
    have to connect table
    - action_log

--- main.dart
+++ main.dart
@@ -4,24 +4,23 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(
-    url: '[https://yscyyvxduwdfjldjnwus.supabase.co]',
-    anonKey: '[eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'
-        '.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlzY3l5dnhkdXdkZmpsZGpud3VzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjIwNjYxMTQsImV4cCI6MjAzNzY0MjExNH0.22vV2RlrW9TU92Y79SzuOQKX8v8IISBcaHePht-43Q4]',
+    url: 'https://yscyyvxduwdfjldjnwus.supabase.co',  // API URL 수정
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'
+        '.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlzY3l5dnhkdXdkZmpsZGpud3VzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjIwNjYxMTQsImV4cCI6MjAzNzY0MjExNH0.22vV2RlrW9TU92Y79SzuOQKX8v8IISBcaHePht-43Q4',  // anonKey 수정
   );
   runApp(const MyApp());
 }
+
+final supabase = Supabase.instance.client;
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Sprinkler Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomePage(),
+    return const MaterialApp(
+      title: 'Supabase Flutter',
+      home: HomePage(),
     );
   }
 }
@@ -38,10 +37,10 @@ class _HomePageState extends State<HomePage> {
 
   // 각 페이지를 나타내는 위젯들
   static List<Widget> _widgetOptions = <Widget>[
-    FirstPage(),
-    SecondPage(),
-    ThirdPage(),
-    ActionLogPage(),
+    const FirstPage(),
+    const SecondPage(),
+    const ThirdPage(),
+    const ActionLogPage(),
   ];
 
   void _onItemTapped(int index) {
@@ -87,102 +86,72 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
-// FirstPage에 리스트 추가
 class FirstPage extends StatelessWidget {
   const FirstPage({super.key});
 
-  // 더미 데이터 생성
-  final List<Map<String, String>> data = const [
-    {
-      'day': 'Monday',
-      'time': '08:00 AM',
-      'temperature': '20°C',
-      'humidity': '60%'
-    },
-    {
-      'day': 'Tuesday',
-      'time': '09:00 AM',
-      'temperature': '22°C',
-      'humidity': '65%'
-    },
-    {
-      'day': 'Wednesday',
-      'time': '10:00 AM',
-      'temperature': '23°C',
-      'humidity': '55%'
-    },
-    {
-      'day': 'Thursday',
-      'time': '11:00 AM',
-      'temperature': '24°C',
-      'humidity': '50%'
-    },
-    {
-      'day': 'Friday',
-      'time': '12:00 PM',
-      'temperature': '25°C',
-      'humidity': '45%'
-    },
-    {
-      'day': 'Friday',
-      'time': '12:00 PM',
-      'temperature': '25°C',
-      'humidity': '45%'
-    },
-    {
-      'day': 'Friday',
-      'time': '12:00 PM',
-      'temperature': '25°C',
-      'humidity': '45%'
-    },
-    {
-      'day': 'Saturday',
-      'time': '08:00 AM',
-      'temperature': '20°C',
-      'humidity': '60%'
-    },
-  ];
+  Future<List<Map<String, dynamic>>> fetchData() async {
+    final response = await supabase
+        .from('sprinkler_get')
+        .select('day, time, temperature, humidity');
+
+
+    return List<Map<String, dynamic>>.from(response as List);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: data.length,
-      itemBuilder: (context, index) {
-        return Container(
-          margin: const EdgeInsets.all(8.0),
-          padding: const EdgeInsets.all(16.0),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(8.0),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.5),
-                spreadRadius: 2,
-                blurRadius: 5,
-                offset: const Offset(0, 3), // changes position of shadow
-              ),
-            ],
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Day: ${data[index]['day']}', style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                  Text('Time: ${data[index]['time']}', style: const TextStyle(fontSize: 16)),
-                ],
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Text('Temp: ${data[index]['temperature']}', style: const TextStyle(fontSize: 16)),
-                  Text('Humidity: ${data[index]['humidity']}', style: const TextStyle(fontSize: 16)),
-                ],
-              ),
-            ],
-          ),
-        );
+    return FutureBuilder<List<Map<String, dynamic>>>(
+      future: fetchData(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return const Center(child: Text('No Data Found'));
+        } else {
+          final data = snapshot.data!;
+          return ListView.builder(
+            itemCount: data.length,
+            itemBuilder: (context, index) {
+              return Container(
+                margin: const EdgeInsets.all(8.0),
+                padding: const EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Day: ${data[index]['day']}', style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                        Text('Time: ${data[index]['time']}', style: const TextStyle(fontSize: 16)),
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text('Temp: ${data[index]['temperature']}°C', style: const TextStyle(fontSize: 16)),
+                        Text('Humidity: ${data[index]['humidity']}%', style: const TextStyle(fontSize: 16)),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        }
       },
     );
   }
@@ -191,10 +160,70 @@ class FirstPage extends StatelessWidget {
 class SecondPage extends StatelessWidget {
   const SecondPage({super.key});
 
+  Future<List<Map<String, dynamic>>> fetchData() async {
+    final response = await supabase
+        .from('sprinkler_get2')
+        .select('day, time, temperature, humidity');
+
+
+    return List<Map<String, dynamic>>.from(response as List);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Second Page'),
+    return FutureBuilder<List<Map<String, dynamic>>>(
+      future: fetchData(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return const Center(child: Text('No Data Found'));
+        } else {
+          final data = snapshot.data!;
+          return ListView.builder(
+            itemCount: data.length,
+            itemBuilder: (context, index) {
+              return Container(
+                margin: const EdgeInsets.all(8.0),
+                padding: const EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Day: ${data[index]['day']}', style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                        Text('Time: ${data[index]['time']}', style: const TextStyle(fontSize: 16)),
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text('Temp: ${data[index]['temperature']}°C', style: const TextStyle(fontSize: 16)),
+                        Text('Humidity: ${data[index]['humidity']}%', style: const TextStyle(fontSize: 16)),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        }
+      },
     );
   }
 }
@@ -202,10 +231,70 @@ class SecondPage extends StatelessWidget {
 class ThirdPage extends StatelessWidget {
   const ThirdPage({super.key});
 
+  Future<List<Map<String, dynamic>>> fetchData() async {
+    final response = await supabase
+        .from('sprinkler_get3')
+        .select('day, time, temperature, humidity');
+
+
+    return List<Map<String, dynamic>>.from(response as List);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Third Page'),
+    return FutureBuilder<List<Map<String, dynamic>>>(
+      future: fetchData(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return const Center(child: Text('No Data Found'));
+        } else {
+          final data = snapshot.data!;
+          return ListView.builder(
+            itemCount: data.length,
+            itemBuilder: (context, index) {
+              return Container(
+                margin: const EdgeInsets.all(8.0),
+                padding: const EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Day: ${data[index]['day']}', style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                        Text('Time: ${data[index]['time']}', style: const TextStyle(fontSize: 16)),
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text('Temp: ${data[index]['temperature']}°C', style: const TextStyle(fontSize: 16)),
+                        Text('Humidity: ${data[index]['humidity']}%', style: const TextStyle(fontSize: 16)),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        }
+      },
     );
   }
 }
